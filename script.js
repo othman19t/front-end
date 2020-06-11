fda6c0c71bf86abb9674a9b49f15dcc00add8c3e
othman19t <othman19t@gmail.com>
2020-06-11 12:11:41 -0700

    second prgrass bar

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ setInterval(()=>{
     const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0; //convert value to float type
     prograssPar.style.setProperty("--width", width +1); // change value of the width varaible 
 }, 50)
+
+// second way for prograss bar
+function filbar(second){
+    const bar = document.getElementById("bar2");
+    bar.style.transition = `${second}s linear width`;
+    bar.style.width = "100%";
+    
+    
+}
